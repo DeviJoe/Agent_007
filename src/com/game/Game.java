cd883ef8b09aee1e30793bb0c523e5f8e24b50fb
Devijoe <lewa.khakimov@mail.ru>
2020-01-14 20:49:36 +0300

    Initial commit

---
+++ src/com/game/Game.java
@@ -0,0 +1,13 @@
+package com.game;
+
+/**
+ * Agent007
+ * http://igroflot.ru/flash_games_31.htm
+ * @author Devijoe (https://github.com/DeviJoe)
+ */
+public class Game {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/game/GameLogic.java
@@ -0,0 +1,7 @@
+package com.game;
+
+/**
+ * Класс отвечает за игровую логику
+ */
+public class GameLogic {
+}
